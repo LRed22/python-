a086308448ab041efc1982936f421f659ad4af44
lovet <lovettaredd@gmail.com>
2022-03-01 00:27:28 -0500

    added list, and sort

--- sequences1.py
+++ sequences1.py
@@ -1,6 +1,27 @@
+from typing import Tuple
+
+
 name = "Love"
 print(name[3])
 names = ["Love", "Rita", "Mercy"]
 print(names[2])
+
+#Tuple/cocrdinate
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0 , 20.0) 
+
+# Data Structures
+#list - sequence of mutable values
+#tuple- sequence of innutable values
+#set - collection of unique values
+#dict- collection of key value pairs
+#Define a list of names
+names = ["nme", "Rone", "Weath", "Ginny"]
+#print(name[2])
+names.append("Lovco")
+names.sort()
+print(names)
 
 
