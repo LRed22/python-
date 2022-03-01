565c4331ea9dd8246e434e4a82603bf8065441cf
lovet <lovettaredd@gmail.com>
2022-02-28 23:36:58 -0500

    added f string

---
+++ lists.py
@@ -0,0 +1,16 @@
+#Data Structures#
+#list - sequence of mutable values#
+#tuples - sequence of immutable values
+#set - collection of unique values#
+#dict - collecton of key-value pairs
+
+#Define a listList
+names = ["Harry", "Ron", "Hermione","Ginny"]
+print(names)
+print(names[0])
+
+#Append add value to the end of the list
+#Sorting-sorting ascending and descending 
+names.append("Draco")
+names.sort()
+print(names)
