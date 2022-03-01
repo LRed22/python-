4a82d89556f3434be1255ffa086f4b48a909da1c
lovet <lovettaredd@gmail.com>
2022-03-01 11:35:17 -0500

    edited range

--- loops.py
+++ loops.py
@@ -1,12 +1,14 @@
 from unicodedata import name
 
 
-for i in [0,1,2,3,4,5]:
-    print(i)
+#for i in [0,1,2,3,4,5]:
+ #   print(i)
 # Using range
-for i in range(6):
-    print(i)
+#for i in range(6):
+ #   print(i)
 #List
 names = ["Harry", "Ndsy","Morris"]
 for name in names:
     print(name)
+for character in name:
+    print(character)
