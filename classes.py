104a7a85a636d17023926914a3e088d1fd852a56
lovet <lovettaredd@gmail.com>
2022-03-03 01:05:08 -0500

    more examples on classes

--- classes.py
+++ classes.py
@@ -5,4 +5,25 @@ class Point():
 p = Point(2,8)
 print(p.x)
 print(p.y)
+   
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        #creating list
+        self.passengers = []
         
+        
+    def add_passenger(self, name):
+        if not self.open_seats():
+            self.passengers.append(name)
+            
+        return True
+        
+    def open_seats(self):
+        return self.capacity-len(self.passengers)
+
+Flight = Flight(3)
+
+people = ["Harry", "Bobbyw", "Harmojnon"]
+
+          
